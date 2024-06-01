d96d92c86af03696e1700cd9d9ecc5f7a0be0e1c
yanggu0t <zxc225751@gmail.com>
2024-06-02 00:32:30 +0800

    feat: add cors settings

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 import { Hono } from "hono";
 import { Moe } from "./services/api";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 const moe = new Moe();
+
+// 使用 CORS 中间件，允许来自 anime1.work 和 localhost 的请求
+app.use(
+  "*",
+  cors({
+    origin: (origin) => {
+      if (
+        origin === "https://anime1.work" ||
+        origin?.startsWith("http://localhost")
+      ) {
+        return origin;
+      }
+      return null; // 如果 origin 不匹配，拒绝请求
+    },
+    allowMethods: ["POST", "GET"],
+    exposeHeaders: ["Content-Length"],
+    // maxAge: 600,
+    credentials: true,
+  })
+);
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -10,7 +31,7 @@ app.get("/", (c) => {
 
 console.log(`================${Date()}===============`);
 
-app.post("/v1", async (c) => {
+app.post("/v1/url", async (c) => {
   try {
     const req = await c.req.json();
     const { url } = req;
